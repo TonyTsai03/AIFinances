a82e08448e609e8768c8735304ee08a1b236d914
Tony <caizongyou0319@gmail.com>
2024-10-08 16:15:18 +0800

    connected success

--- aifinances/aifinances/settings.py
+++ aifinances/aifinances/settings.py
@@ -11,11 +11,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+DATABASE_URL = os.environ.get('DATABASE_URL')
+print(DATABASE_URL)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -74,12 +80,12 @@ WSGI_APPLICATION = 'aifinances.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default = DATABASE_URL,
+        conn_max_age = 600,
+        ssl_require = True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
---
+++ aifinances/prediction/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+import finlab
+# Create your models here.
+finlab.login('r0K9y4lF4EhgdSIjBVE5vY7ZKMhXqNr/N0yWFGz/keCB1a87U4N1xykyUlLu9B7S#vip_m')
